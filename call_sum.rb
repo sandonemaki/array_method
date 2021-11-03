d2b95411f4ebf37a456ad015528a09791df286da
kamuramaki@gmail.com <kamuramaki@gmail.com>
2021-11-03 18:06:36 +0900

    ./array_sample_bubblesortを呼び出した

--- call_sum.rb
+++ call_sum.rb
@@ -1,6 +1,14 @@
-require './array_sample_sum'
-a = ArraySampleSum.new([1, 2, 3])
-puts a.class
-puts a.new_sum.class
-puts a.new_sum.to_s
-a.sum
+# require './array_sample_sum'
+# a = ArraySampleSum.new([1, 2, 3])
+# puts a.class
+# puts a.new_sum.class
+# puts a.new_sum.to_s
+# a.sum
+
+
+require './array_sample_bubblesort'
+a = ArraySampleBubblesort.new([1, 10, 3, 5, 9])  
+# ArraySampleBubblesortのインスタンス化したオブジェクトがaでその中の変数として@arrayがある
+# オブジェクトの中の変数をメンバという
+
+a.bubble_sort('11')
