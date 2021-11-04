7ac8953f618e7de2ef93e999c8a86278d5af71d7
kamuramaki@gmail.com <kamuramaki@gmail.com>
2021-11-04 21:34:51 +0900

    any?の戻値を出力

--- call_sum.rb
+++ call_sum.rb
@@ -6,9 +6,28 @@
 # a.sum
 
 
-require './array_sample_bubblesort'
-a = ArraySampleBubblesort.new([1, 10, 3, 5, 9])  
-# ArraySampleBubblesortのインスタンス化したオブジェクトがaでその中の変数として@arrayがある
-# オブジェクトの中の変数をメンバという
+# require './array_sample_bubblesort'
+# a = ArraySampleBubblesort.new([1, 10, 3, 5, 9])  
+# # ArraySampleBubblesortのインスタンス化したオブジェクトがaでその中の変数として@arrayがある
+# # オブジェクトの中の変数をメンバという
 
-a.bubble_sort('11')
+# a.bubble_sort('11')
+
+
+# require './array_sample_any?'
+# a = ArraySampleAny.new([1, 2, 3])
+# result = a.any? #a.any?の返り値をresultに代入する
+# puts result
+
+
+require './array_sample_any?'
+a = ArraySampleAny.new([1, 2, 3])
+#a.any?の返り値をresultに代入する
+#result = a.any? {|v| v > 2 }
+
+result = a.any? {|v| v > 4}
+puts result
+
+# block = Proc.new {|v| v > 4}
+# result = a.any?(&block)
+# puts result
